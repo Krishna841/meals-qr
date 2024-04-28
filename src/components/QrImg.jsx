7f6cbf3c0250d6f0b8455f5f662745d4740c9e31
Krishna841 <65186847+Krishna841@users.noreply.github.com>
2024-04-28 23:56:01 +0530

    minor fixes

--- src/components/QrImg.jsx
+++ src/components/QrImg.jsx
@@ -1,22 +1,29 @@
-import React from "react";
+import { WhatsappShareButton } from "react-share";
 import WA from "../assets/wa-icon.svg";
+import { getWhatsAppUrl } from "@phntms/react-share";
 
 function QrImg({ data }) {
+  //   const handleShorten = async () => {
+  //     try {
+  //       const response = await axios(
+  //         `https://api.shrtco.de/v2/shorten?url=${userInput}`
+  //       );
+  //       setShortenedLink(response.data.result.full_short_link);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   };
+
   return (
     <div className="flex-shrink-0 snap-center items-center justify-center relative">
       <img src={data.qrUrl} alt="qr" className="w-[277px] h-[428px] " />
-      <a
-        href={
-          `whatsapp://send?text=${encodeURIComponent("QR")}&amp;` +
-          `url=${encodeURIComponent(data.qrUrl)}`
-        }
-      >
+      <WhatsappShareButton url={data.qrUrl}>
         <img
           src={WA}
           alt="wa-icon"
           className="absolute right-20 bottom-28 h-[30px] w-[30px] rounded-md"
         />
-      </a>
+      </WhatsappShareButton>
     </div>
   );
 }
--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import Meals from "./Meals";
+import QrImg from "./QrImg";
+import moment from "moment";
 
-function Today() {
-  return <div>Today</div>;
+function Today({ setType, type }) {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("https://pos-api.effortsnode.srmd.org/v1/items/getMyPasses", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        param: "4c788c407d64217d89a7d934abe356cb",
+        startDate: moment(new Date()).format("YYYY-MM-DD"),
+        endDate: moment(new Date()).format("YYYY-MM-DD"),
+        // startDate: "2024-04-22",
+        // endDate: "2024-04-22",
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setData(result.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <Meals setType={setType} />
+      <div className="flex items-center justify-start mt-5 overflow-x-scroll no-scrollbar px-5 gap-5">
+        {data && Object.keys(data).length ? (
+          data
+            ?.filter((item) => !type || item.type === type)
+            .map((item) => <QrImg data={item} key={item.id} />)
+        ) : (
+          <div className="flex items-center">
+            No passes available for today.
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Today;
